ffe35c907afce68b7c9c2817112b0911b3c055e8
dpajek <dan.pajek@gmail.com>
2020-01-29 22:26:09 -0500

    1 file modified -- formatting change for title

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'hello',
   template: `<h1>{{name}} Habit Tracker</h1>`,
-  styles: [`h1 { font-family: Lato; }`]
+  styles: [``]
 })
 export class HelloComponent  {
   @Input() name: string;
