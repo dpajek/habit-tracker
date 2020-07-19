155e4bfa8555cdebb7fe8ffe540bd283747bad09
dpajek <dan.pajek@gmail.com>
2020-07-19 14:38:38 -0400

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Router } from '@angular/router';
 import { AuthService } from './auth.service';
 
 @Component({
@@ -6,10 +8,20 @@ import { AuthService } from './auth.service';
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
 
-constructor (public auth: AuthService) {}
+constructor (public auth: AuthService,
+    public router: Router) {}
 
   //name = 'Daniel\'s';
   name = '';
+
+
+ngOnInit() {
+    if(this.auth.isLoggedIn === true) {
+      console.log('Logged in, redirect to habits page')
+      this.router.navigate(['/habits']);
+    }
+  }
+
 }
