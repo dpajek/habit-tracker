c0e73074eb76ded27ed140d428da300c992fc1df
dpajek <dan.pajek@gmail.com>
2020-07-28 21:10:15 -0400

    2 files added

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDKqbWflc7EULr8beTlTyb2TQ1XW-0p-Fk",
+    authDomain: "habit-tracker-38e6c.firebaseapp.com",
+    databaseURL: "https://habit-tracker-38e6c.firebaseio.com",
+    projectId: "habit-tracker-38e6c",
+    storageBucket: "habit-tracker-38e6c.appspot.com",
+    messagingSenderId: "199360809587"
+  }
+};
